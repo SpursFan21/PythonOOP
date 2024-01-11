7854abd863e27b3db969a0c25cfc983f7f36ee5b
Duncan Turner <duncanfrederic@gmail.com>
2024-01-11 17:43:44 +1300

    QuestionModel update

--- QuestionModelClass.py
+++ QuestionModelClass.py
@@ -1,4 +1,6 @@
+# QuestionModelClass.py
+
 class QuestionModel:
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
+    def __init__(self, question_data):
+        self.text = question_data["question"]
+        self.answer = question_data["correct_answer"]
